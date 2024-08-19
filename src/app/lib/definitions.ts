e383cb5a00ca31965fc7a961ff1d411585cd7b11
cormickwhite <cormickwhite@gmail.com>
2024-08-19 13:52:50 -0500

    add spell and character types

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,138 @@
+export type Class =
+  | "barbarian"
+  | "bard"
+  | "cleric"
+  | "druid"
+  | "fighter"
+  | "monk"
+  | "paladin"
+  | "ranger"
+  | "rogue"
+  | "sorcerer"
+  | "warlock"
+  | "wizard";
+
+export type Race =
+  | "dwarf"
+  | "elf"
+  | "halfling"
+  | "human"
+  | "dragonborn"
+  | "gnome"
+  | "half-elf"
+  | "half-orc"
+  | "tiefling";
+
+export type School =
+  | "abjuration"
+  | "conjuration"
+  | "divination"
+  | "enchantment"
+  | "evocation"
+  | "illusion"
+  | "necromancy"
+  | "transmutation";
+
+export type CastingTime =
+  | "1 action"
+  | "1 bonus action"
+  | "1 reaction"
+  | "1 minute"
+  | "10 minutes"
+  | "1 hour"
+  | "8 hours"
+  | "12 hours"
+  | "24 hours";
+
+export type Ability =
+  | "strength"
+  | "dexterity"
+  | "constitution"
+  | "intelligence"
+  | "wisdom"
+  | "charisma";
+
+export type SpellType = "melee" | "ranged";
+
+export type DamageType =
+  | "acid"
+  | "bludgeoning"
+  | "cold"
+  | "fire"
+  | "force"
+  | "lightning"
+  | "necrotic"
+  | "piercing"
+  | "poison"
+  | "psychic"
+  | "radiant"
+  | "slashing"
+  | "thunder"
+  | "shortbow"
+  | "longbow"
+  | "one-handed melee attacks"
+  | "unarmed attacks"
+  | "natural attacks"
+  | "melee weapon attacks";
+
+export type Condition =
+  | "blinded"
+  | "charmed"
+  | "deafened"
+  | "exhaustion"
+  | "frightened"
+  | "grappled"
+  | "incapacitated"
+  | "invisible"
+  | "paralyzed"
+  | "petrified"
+  | "poisoned"
+  | "prone"
+  | "restrained"
+  | "stunned"
+  | "unconscious";
+
+export type AreaOfEffect = "cone" | "cube" | "cylinder" | "line" | "sphere";
+
+export type Spell = {
+  id: string;
+  name: string;
+  description: string;
+  class: Class[];
+  level: number | "cantrip";
+  school: School;
+  casting_time: CastingTime;
+  duration: "instantaneous";
+  area_of_effect: {
+    shape?: AreaOfEffect;
+    range?: number;
+  };
+  save: Ability;
+  type: SpellType;
+  damage_type: DamageType;
+  conditions?: Condition[];
+  components?: {
+    verbal: boolean;
+    somatic: boolean;
+    material: boolean;
+    materials?: string[];
+  };
+  concentration: boolean;
+  ritual: boolean;
+};
+
+export type Character = {
+  id: string;
+  name: string;
+  class: Class;
+  level: number;
+  race: Race;
+  ability_scores: {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+  };
+};
